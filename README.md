0a6b11270445a8b983888f42ebfd5dfaeeccb79a
Jaber-Saed <jabrsalm449@gmail.com>
2024-02-02 23:42:05 +0300

    🐛 (server.ts): Fix variable case for port  ✨ (server.ts): Add support for environment variable PORT

---
+++ README.md
@@ -0,0 +1,56 @@
+# Personal Portfolio
+
+![Portfolio Preview](/public/gitThem.png)
+
+## Introduction
+
+This is a personal portfolio website built using ReactJS, Vite, and other technologies. It serves as a showcase of my skills, projects, and personal information.
+
+## Technologies Used
+
+- **ReactJS**: A JavaScript library for building user interfaces.
+- **Vite**: A fast, opinionated web development build tool that serves your code via native ES Module imports.
+- **[List other technologies used]**
+
+## Features
+
+- Responsive design for a seamless experience across devices.
+- Sections for showcasing projects, skills, and personal information.
+- [List other features]
+
+## Getting Started
+
+Follow these instructions to set up and run the project on your local machine.
+
+### Prerequisites
+
+- Node.js: Make sure Node.js is installed on your machine. You can download it from [here](https://nodejs.org/).
+
+### Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/Jaber-Saed/3d-protfoluo.git
+
+2. Change to the project directory:
+
+   ```bash
+   cd your-portfolio
+
+
+3. Install dependencies::
+
+   ```bash
+   npm install
+
+4. Start the development server:
+
+   ```bash
+   npm run dev
+
+
+5. build the project :
+
+   ```bash
+   npm run build
---
+++ src/components/Header/Header.js
@@ -0,0 +1,48 @@
+import Link from 'next/link';
+import React from 'react';
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { DiCssdeck } from 'react-icons/di';
+
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+
+const Header = () =>  (
+  <Container>
+    <Div1>
+      <Link href="/" style={{ display: 'flex', alignItems: 'center', color:"white" }}>
+
+        <DiCssdeck size="3rem" /> <span>Portfolio</span>
+
+      </Link>
+    </Div1>
+    <Div2>
+      <li>
+        <Link href="#projects" legacyBehavior>
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#tech" legacyBehavior>
+          <NavLink>Technologies</NavLink>
+        </Link>
+      </li>        
+      <li>
+        <Link href="#about" legacyBehavior>
+          <NavLink>About</NavLink>
+        </Link>
+      </li>        
+    </Div2>
+      <Div3>
+        <SocialIcons href="https://google.com">
+          <AiFillGithub size="3rem" />
+        </SocialIcons>
+        <SocialIcons href="https://google.com">
+          <AiFillLinkedin size="3rem" />
+        </SocialIcons>
+        <SocialIcons href="https://google.com">
+          <AiFillInstagram size="3rem"/>
+        </SocialIcons>
+      </Div3>
+    </Container>
+);
+
+export default Header;
